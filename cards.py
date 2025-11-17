7dfc830ecfc26187844e71736c5b68626b5a4857
shubham <shubham.mahapatraa@gmail.com>
2025-11-17 19:12:31 +0530

    gamelogic-2
    Added game logic for rounds and player card management.

--- cards.py
+++ cards.py
@@ -1,9 +1,11 @@
 import random
 
+
 suits = ('Spades', 'Hearts', 'Diamonds', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9,
            'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
 
 class Card():
 
@@ -68,3 +70,36 @@ new_deck.shuffle()
 for x in range(26):
     player_one.add_cards(new_deck.deal_one())
     player_two.add_cards(new_deck.deal_one())
+
+
+#MORE LOGIC
+
+
+game_on = True
+
+round_num = 0
+
+while game_on:
+    
+    round_num += 1
+    print(f"Round {round_num}")
+
+    if len(player_one.all_cards) == 0:
+        print("Player One, out of cards! Player Two won!")
+        game_on = False
+        break
+
+    if len(player_two.all_cards) == 0:
+        print("Player Two, out of cards! Player One won!")
+        game_on = False
+        break
+
+           
+#NEW ROUND BEGINS
+
+           
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
