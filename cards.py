0aa158c3a2295efab6395a71d352070cb08b5ce1
shubham <shubham.mahapatraa@gmail.com>
2025-11-19 23:40:53 +0530

    meh
    Added a comment header for class definitions.

--- cards.py
+++ cards.py
@@ -7,6 +7,9 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
            'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 
+#CREATING ALL CLASSES
+
+
 class Card():
 
     def __init__(self, suit, rank):
