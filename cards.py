61ec674fdf0ea51991179902ed3cade67f98f086
shubham <shubham.mahapatraa@gmail.com>
2025-11-21 12:51:01 +0530

    Add war logic to handle card game ties
    Implement war logic for card game between players.

--- cards.py
+++ cards.py
@@ -106,3 +106,46 @@ while game_on:
 
     player_two_cards = []
     player_two_cards.append(player_two.remove_one())
+
+
+#AT WAR
+
+
+    at_war = True
+
+    while at_war:
+
+        if player_one_cards[-1].value > player_two_cards[-1].value:
+
+            player_one.add_cards(player_one_cards)
+            player_one.add_cards(player_two_cards)
+
+            at_war = False
+
+        elif player_one_cards[-1].value < player_two_cards[-1].value:
+
+            player_two.add_cards(player_one_cards)
+            player_two.add_cards(player_two_cards)
+
+            at_war = False
+
+        else:
+            print('WAR!')
+
+            if len(player_one.all_cards) < 3:
+                print('Player One unable to declare war.')
+                print('Player Two wins.')
+                game_on = False
+                break
+
+            elif len(player_two.all_cards) < 3:
+                print('Player Two unable to declare war.')
+                print('Player One wins.')
+                game_on = False
+                break
+
+            else:
+                for num in range(3):
+                    player_one_cards.append(player_one.remove_one())
+                    player_two_cards.append(player_two.remove_one())
+
